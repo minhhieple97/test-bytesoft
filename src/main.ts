eec3fd395ca90a588001211dcd484cce2f4704ca
hieple97 <42188983+minhhieple97@users.noreply.github.com>
2022-07-07 09:55:30 +0700

    update port env

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,16 @@ import { AppModule } from './app.module';
 const cookieSession = require('cookie-session');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieSession({
-    keys:['thisissecret']
-  }))
+  app.use(
+    cookieSession({
+      keys: ['thisissecret'],
+    }),
+  );
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist:true
-    })
-  )
-  await app.listen(3000);
+      whitelist: true,
+    }),
+  );
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
